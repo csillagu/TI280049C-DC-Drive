83099e7a587ad5a429e73521ef98100de117989b
Ujvari Andras <ujvariandris@gmail.com>
2022-02-28 17:11:55 +0100

    leiras

--- adc/adc_board.c
+++ adc/adc_board.c
@@ -17,7 +17,6 @@ void Board_init()
     ADC_init();
     INTERRUPT_init();
 
-
     EDIS;
 }
 
@@ -64,6 +63,43 @@ void EPWM_init(){
     //myEPWM5 initialization
     //myEPWM6 initialization
 }
+void ADC_init(){
+
+    ADC_setOffsetTrimAll(ADC_REFERENCE_INTERNAL,ADC_REFERENCE_3_3V); //3.3 internal volt ref
+    ADC_setPrescaler(myADC0_BASE, ADC_CLK_DIV_2_0); // valami clockot osztunk 2vel
+    ADC_setInterruptPulseMode(myADC0_BASE, ADC_PULSE_END_OF_CONV); //akkor lesz interrrupt, amikor megvannak a dolgaink
+    ADC_enableConverter(myADC0_BASE); //beizzitjuk es varunk hogy folporogjon
+    DEVICE_DELAY_US(5000);
+
+    ADC_disableBurstMode(myADC0_BASE); //?
+    ADC_setSOCPriority(myADC0_BASE, ADC_PRI_ALL_ROUND_ROBIN); // ennek tul sok ertelme nincs
+    ADC_setupSOC(myADC0_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 8U); //hozzakapcsoljuk az adc0base-hez a 0.soc-ot, es epwm trigger, meg a 0. port
+    ADC_setInterruptSOCTrigger(myADC0_BASE, ADC_SOC_NUMBER0, ADC_INT_SOC_TRIGGER_NONE); // az interrupt utan ne triggetelodjon mas
+
+    ADC_setInterruptSource(myADC0_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER1); // ha a soc1 vegeter kuldje el az interrupt 1-et
+    ADC_enableInterrupt(myADC0_BASE, ADC_INT_NUMBER1); //engedelyezzuk
+    ADC_clearInterruptStatus(myADC0_BASE, ADC_INT_NUMBER1); //cleareljuk
+    ADC_disableContinuousMode(myADC0_BASE, ADC_INT_NUMBER1); // csak clear utan johet a kovetkezo interrupt
+
+}
+// jo kerdes
+void ASYSCTL_init(){
+    // asysctl initialization
+    // Disables the temperature sensor output to the ADC.
+    ASysCtl_disableTemperatureSensor();
+    // Set the analog voltage reference selection to internal.
+    ASysCtl_setAnalogReferenceInternal( ASYSCTL_VREFHIA | ASYSCTL_VREFHIB | ASYSCTL_VREFHIC );
+    // Set the internal analog voltage reference selection to 1.65V.
+    ASysCtl_setAnalogReference1P65( ASYSCTL_VREFHIA | ASYSCTL_VREFHIB | ASYSCTL_VREFHIC );
+}
+void INTERRUPT_init(){
+
+    // Interrupt Setings for INT_myADC0_1
+    Interrupt_register(INT_myADC0_1, &adcA1ISR); // az a fuggveny fogja interruptolni
+    Interrupt_enable(INT_myADC0_1);
+}
+
+
 
 
 
